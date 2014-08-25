14e3bbe8112e21f81d52a42d023aa73f4c2d2e00
Sam Neubardt <samneubardt@gmail.com>
2014-08-24 21:52:47 -0400

    Add service definition & java source

--- dog-shelter/dog-shelter-core/src/thrift/schema.thrift
+++ dog-shelter/dog-shelter-core/src/thrift/schema.thrift
@@ -1,5 +1,13 @@
 namespace java dog_shelter.thrift
 
+struct DogInfo {
+    1: string breed
+    2: string name
+    3: bool houseTrained
+    4: bool beautiful
+}
+
 service DogShelter {
-    // TODO
+    void addDog(1: string breed 2: string name 3: bool houseTrained)
+    list<DogInfo> listDogs()
 }
